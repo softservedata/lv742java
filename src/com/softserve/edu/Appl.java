48b665fc46ea21508f3f039adc75a8d386d9fca5
Yaroslav <garasym@gmail.com>
2022-12-29 19:40:42 -0800

    Simple Class Added

--- src/com/softserve/edu/Appl.java
+++ src/com/softserve/edu/Appl.java
@@ -1,7 +1,10 @@
 package com.softserve.edu;
 
+import com.softserve.edu03oop.App03;
+
 public class Appl {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //App03 app03 = new App103();
     }
 }
--- src/com/softserve/edu03oop/App03.java
+++ src/com/softserve/edu03oop/App03.java
@@ -1,4 +1,46 @@
 package com.softserve.edu03oop;
 
 public class App03 {
+    private int i; // 0 by default;
+    private static int j = 432;
+
+    static {
+        System.out.println("\tstatic block start, j = " + j);
+        j = 789;
+        System.out.println("\tstatic block done, j = " + j);
+    }
+
+    // Overload
+    public App03() {
+        System.out.println("\tconstructor App03() start, i = " + i);
+        i = -1;
+        System.out.println("\tconstructor App03() done, i = " + i);
+    }
+
+    // Overload
+    private App03(int i) {
+        System.out.println("\tconstructor App03(int i) start, this.i = " + this.i);
+        this.i = i;
+        System.out.println("\tconstructor App03(int i) done, this.i = " + this.i);
+    }
+
+    public void myPrint() {
+        System.out.println("i = " + i);
+        j = i;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("main start j = " + j);
+        //
+        App03 app1 = new App03();
+        app1.i = 123;
+        //
+        App03 app2 = new App03(456);
+        //app2.i = 456;
+        //
+        app1.myPrint();
+        System.out.println("\tj = " + j);
+        app2.myPrint();
+        System.out.println("\tj = " + j);
+    }
 }
