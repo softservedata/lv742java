b6b4b3112ef96c69361cc7a1c00cb5763057864c
Kostya Kozub <kostyakozyb@gmail.com>
2023-01-10 17:14:04 +0200

    Homework 03

---
+++ src/com/softserve/edu04/App04.java
@@ -0,0 +1,22 @@
+package com.softserve.edu04;
+
+public class App04 {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.input("Kostya", "Kozub", 2005);
+        Person person2 = new Person();
+        person2.input("Ivan", "Kravchuk", 1979);
+        Person person3 = new Person();
+        person3.input("Stepan", "Bodnar", 1995);
+        Person person4 = new Person();
+        person4.input("Mykola", "Ilashuk", 2007);
+        Person person5 = new Person();
+        person5.input("Viktor", "Galushka", 1984);
+        person5.changeName("Katya", "Lisna");
+        person1.output();
+        person2.output();
+        person3.output();
+        person4.output();
+        person5.output();
+    }
+}
---
+++ src/com/softserve/edu04/Person.java
@@ -0,0 +1,72 @@
+package com.softserve.edu04;
+
+import java.time.LocalDate;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int birthYear;
+
+    public Person() {
+        firstName = "DefaultFirstName";
+        lastName = "DefaultLastName";
+        birthYear = 2000;
+    }
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public int getAge(){
+        return LocalDate.now().getYear() - birthYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName: " + this.getFirstName() +
+                ", lastName: " + this.getLastName() +
+                ", birthYear: " + this.getBirthYear() +
+                ", age: " + getAge() +
+                '}';
+    }
+
+    public void input(String fn, String ln, int bd){
+        this.setFirstName(fn);
+        this.setLastName(ln);
+        this.setBirthYear(bd);
+    }
+
+    public void output(){
+        System.out.println(this);
+    }
+
+    public void changeName(String fn, String ln){
+        this.firstName = fn;
+        this.lastName = ln;
+    }
+}
