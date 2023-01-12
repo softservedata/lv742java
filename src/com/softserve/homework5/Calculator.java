3a33ec72e5b8756d212683a592015f4f11793c35
tetianadivnych <divnatany17@gmail.com>
2023-01-12 17:08:34 +0200

    add homework 5

--- src/com/softserve/homework5/Calculator.java
+++ src/com/softserve/homework5/Calculator.java
@@ -1,6 +1,7 @@
 package com.softserve.homework5;
 
 public class Calculator {
+
     public static void matchMonthDays() {
         int input = InputReader.readMonth();
         int[] monthDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
