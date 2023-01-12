d81ee91086bfe7437f8af01bda878078c3f3acbf
bebebe <hahaha@gmail.com>
2023-01-12 19:16:30 +0200

    Java Inheritance.

---
+++ src/com/softserve/edu07inher/Base.java
@@ -0,0 +1,10 @@
+package com.softserve.edu07inher;
+
+public abstract class Base { // extends Object { // extends Object by default
+
+    public Base() {
+        System.out.println("constructor Base()");
+    }
+
+    public abstract int getI();
+}
